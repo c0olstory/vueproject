96a3fe94d6efe6d52d0f4df7ce6b1b8cdc1c6987
coolstory <vlask1n@yandex.ru>
2021-05-19 23:05:17 +0300

    lesson-3

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id='app' :class="[$style.app]">
+    <h1 :class="[$style.app__header]">My personal costs</h1>
+    <main>
+      <ButtonNewCost @showNewCostForm ="showForm"/>
+      <PaymentForm @add="onDataAdded" v-if="showFormCost"/>
+      <PaymentsList :items="paymentsList" />
+    </main>
+  </div>
+</template>
+
+<script>
+import PaymentsList from './components/PaymentsList'
+import PaymentForm from './components/PaymentForm'
+import ButtonNewCost from './components/Button'
+
+export default {
+  name: 'App',
+  components: {
+    PaymentsList,
+    PaymentForm,
+    ButtonNewCost
+  },
+  data () {
+    return {
+      showFormCost: false,
+      paymentsList: [
+        {
+          id: 1,
+          date: '20.03.2020',
+          category: 'Food',
+          price: 169
+        },
+        {
+          id: 2,
+          date: '21.03.2020',
+          category: 'Navigation',
+          price: 50
+        },
+        {
+          id: 3,
+          date: '22.03.2020',
+          category: 'Sport',
+          price: 450
+        }
+      ]
+    }
+  },
+  methods: {
+    onDataAdded (data) {
+      this.paymentsList.push(data)
+    },
+    showForm (value) {
+      this.showFormCost = value
+    }
+  }
+}
+</script>
+
+<style lang='scss' module>
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: "Arial";
+  color: #000;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="[$style.button]">
+    <button :class="[$style.button__btn]" @click="showAddForm">Add new cost <span>+</span></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    showAddForm () {
+      this.show = !this.show
+      this.$emit('showNewCostForm', this.show)
+    }
+  }
+}
+</script>
+
+<style lang='scss' module>
+.button {
+  margin-bottom: 20px;
+  &__btn {
+    font-weight: 100;
+    border: none;
+    font-size: 18px;
+    color: #fff;
+    background-color: #00a89b;
+    box-shadow: 2px 2px 5px 1px rgb(173, 173, 173);
+    padding: 10px 0px;
+    text-transform: uppercase;
+    width: 50%;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 1s ease;
+  }
+}
+</style>
