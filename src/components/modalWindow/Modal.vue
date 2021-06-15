8866799a8af993d892e64f3f548d21e20b107875
coolstory <vlask1n@yandex.ru>
2021-06-15 14:26:46 +0300

    test

---
+++ src/components/modalWindow/Modal.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="[$style.modal]">
+    <div :class="[$style.modal__overlay]"></div>
+    <div :class="[$style.modal__content]">
+      <PaymentForm
+        :class="[$style.modal__form]"
+        v-if="name === 'PaymentForm'"
+        :idItem="settings && settings.id"
+        btn="save"
+      />
+      <button
+        :class="[$style.modal__close]"
+        @click="onClose"
+      >close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import PaymentForm from '../PaymentForm'
+
+export default {
+  components: {
+    PaymentForm
+  },
+  props: {
+    name: String,
+    settings: Object
+  },
+  methods: {
+    onClose () {
+      this.$modal.close()
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+.modal {
+  position: absolute;
+  // border: 1px solid #000;
+  width: 100%;
+  height: 100vh;
+  &__overlay {
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: rgba(50,50,50,0.5);
+  }
+  &__content {
+    z-index: 10;
+    display: flex;
+    position: relative;
+    // transform: translate(50%, 50%);
+    background-color: #fff;
+    box-shadow: 0px 0px 5px 1px;
+    top: 50%;
+    left: 50%;
+    width: 400px;
+    transform: translate(-50%, -50%);
+  }
+  &__form {
+    transform: translate(-50%, -50%);
+  }
+  &__close {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+}
+</style>
---
+++ src/page/PageDashboard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row>
+    <v-col>
+      <div class="text-h5 text-md-h3 my-8">My personal Costs</div>
+      <ButtonNewCost @showNewCostForm ="showForm"/>
+      <PaymentForm @add="onDataAdded" v-if="showFormCost" :btn="'add'"/>
+      <PaymentsList />
+    </v-col>
+    <v-col>Diagram</v-col>
+  </v-row>
+  <!-- <div>
+    <ButtonNewCost @showNewCostForm ="showForm"/>
+    <PaymentForm @add="onDataAdded" v-if="showFormCost" :btn="'add'"/>
+    <PaymentsList />
+  </div> -->
+</template>
+
+<script>
+import PaymentsList from '../components/PaymentsList'
+import PaymentForm from '../components/PaymentForm'
+import ButtonNewCost from '../components/Button'
+
+import { mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      showFormCost: false
+    }
+  },
+  components: {
+    PaymentsList,
+    PaymentForm,
+    ButtonNewCost
+  },
+  methods: {
+    ...mapActions([
+      'fetchData'
+    ]),
+    onDataAdded (data) {
+      this.paymentsList.push(data)
+    },
+    showForm (value) {
+      this.showFormCost = value
+    }
+  },
+  mounted () {
+    this.fetchData()
+  }
+}
+</script>
+
+<style>
+
+</style>
